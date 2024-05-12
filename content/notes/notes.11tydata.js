95572a9e1f408454563f5bb68bc1aa7128c36b4b
cconrad <584491+cconrad@users.noreply.github.com>
2024-05-12 19:18:30 +0200

    Do not rely on "title" being present when generating the single notes

--- content/notes/notes.11tydata.js
+++ content/notes/notes.11tydata.js
@@ -1,5 +1,8 @@
 module.exports = function () {
   return {
+    eleventyComputed: {
+      title: (data) => data.title || data.page.fileSlug,
+    },
     layout: "note.njk", 
     tags: ["notes"]
   };
